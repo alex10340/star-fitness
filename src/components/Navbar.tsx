63c075fefc28ccbf970b47cbf29eb3f86128dd28
alex103040 <alexandre.pir.silva@gmail.com>
2024-09-07 15:45:05 +0100

    misc

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <div className="sticky top-0 z-10 backdrop-blur-md bg-background/80 border-b">
       <div className="relative flex items-center h-fit px-6 py-4 max-w-screen-xl mx-auto">
         <Link href="/" className="flex items-center space-x-2 mr-auto">
-          <Star color="#f43f5e" />
+          <Star className=" text-primary" />
           <p className="font-bold text-lg">Star Fitness</p>
         </Link>
 
